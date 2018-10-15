e61d91d294fcd27dd237c466c3c7eb1e763ba353
Alexander Shumway <alshumway@darwinsmonkey.net>
2018-10-15 17:04:47 -0400

    Initial commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameSession : MonoBehaviour
+{
+
+    //config params
+    [SerializeField] float gameSpeedMin = 0.1f;
+    [SerializeField] float gameSpeedMax = 10f;
+    [Range(0f, 100f)]  [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+
+    private void Awake()
+    {
+
+        int gameSessionCount = FindObjectsOfType<GameSession>().Length;
+        if (gameSessionCount > 1) 
+        {
+
+            Destroy(gameObject);
+
+        }
+
+        else 
+        {
+
+            DontDestroyOnLoad(gameObject);
+
+        }
+    }
+
+    private void Start()
+    {
+
+        scoreText.text = currentScore.ToString();
+
+    }
+
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+        Time.timeScale = gameSpeed;
+
+	}
+
+
+    public void AddToScore() 
+    {
+
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString(); 
+
+    }
+
+    public void ResetGame()
+    {
+
+        Destroy(gameObject);
+
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+
+        return isAutoPlayEnabled;
+
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField] float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+
+        StartCoroutine(delaySplash());
+
+    }
+
+    IEnumerator delaySplash()
+    {
+
+        yield return new WaitForSeconds(autoLoadNextLevelAfter);
+        LoadNextScene();
+
+    }
+
+    public void LoadNextScene()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+
+
+    }
+
+    public void LoadStartScene()
+    {
+
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
+
+
+    }
+
+    public void QuitGame()
+    {
+        //Testing Version Control
+        Application.Quit();
+
+    }
+
+}
