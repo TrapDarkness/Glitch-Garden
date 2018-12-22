ed41215148b26c0994336270a2bebaa2f39a497f
Alexander Shumway <alshumway@darwinsmonkey.net>
2018-12-21 21:13:03 -0500

    Attackers will now attack when colliding with a defender, and fox will jump over the gravestone

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,29 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class Attacker : MonoBehaviour {
 
-    [Range(-1f, 1.5f)] public float walkSpeed;
+    [Range(-1f, 1.5f)]
+    float currentSpeed;
+    GameObject currentTarget;
 
 
 	// Use this for initialization
 	void Start () {
-
-        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
-        myRigidbody.isKinematic = true;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 
 	}
     void OnTriggerEnter2D(Collider2D collision)
     {
 
         Debug.Log(name + "trigger enter");
+
+    }
+
+    public void SetSpeed (float speed)
+    {
+
+        currentSpeed = speed;
+
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+
+        Debug.Log(name + " caused damage: " + damage);
+
+    }
+
+    public void Attack(GameObject obj)
+    {
+
+        currentTarget = obj;
+
 
     }
 
---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Attacker))]
+
+public class Fox : MonoBehaviour
+{
+
+    Animator anim;
+    Attacker attacker;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        attacker = GetComponent<Attacker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+    
+        GameObject obj = collision.gameObject;
+
+        //Leave method if not colliding with defender
+        if (!obj.GetComponent<Defender>())
+        {
+
+            return;
+
+        }
+
+        if (obj.GetComponent<Stone>())
+        {
+
+            anim.SetTrigger("jump trigger");
+
+        } else {
+
+            anim.SetBool("isAttacking", true);
+            attacker.Attack(obj);
+
+        }
+
+            Debug.Log("Fox collided with " + collision);
+
+    }
+
+}
