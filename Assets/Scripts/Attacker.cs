54c030708e06d07552ec9a6feb08bbffc3649e62
Alexander Shumway <alshumway@darwinsmonkey.net>
2018-12-24 12:39:37 -0500

    Attackers can destroy defenders and projectiles will move and rotate.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -9,16 +9,23 @@ public class Attacker : MonoBehaviour {
     [Range(-1f, 1.5f)]
     float currentSpeed;
     GameObject currentTarget;
+    private Animator animator;
 
 
 	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+        if(!currentTarget)
+        {
+            animator.SetBool("isAttacking", false);
+        }
 
 	}
     void OnTriggerEnter2D(Collider2D collision)
@@ -38,7 +45,18 @@ public class Attacker : MonoBehaviour {
     public void StrikeCurrentTarget(float damage)
     {
 
-        Debug.Log(name + " caused damage: " + damage);
+        if (currentTarget)
+        {
+
+            Health health = currentTarget.GetComponent<Health>();
+            if (health)
+            {
+
+                health.DealDamage(damage);
+
+            }
+
+        }
 
     }
 
